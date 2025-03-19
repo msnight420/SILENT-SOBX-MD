aca91a56cf9c4c4b85a68dfcb84c2a4d8cb84821
msnight420 <mswindow187@gmail.com>
2025-03-20 00:16:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "SILENT-SOBX-MD~rJQAxR5L#WJgzKMbTZZMAxFdgN2wh_ZpVD_5C2c7O7rrNUIy2yyM",
-    CAPTION: process.env.CAPTION || "POWERED BY MS_HACKER",
+    SESSION_ID: process.env.SESSION_ID || "SILENT-SOBX-MD~DBow2T5K#Bu5hV-3T3qV7GgLhnT5aJJ5OYq86Pgqz27XeHh-O4wY",
+    CAPTION: process.env.CAPTION || "POWERED BY 5t7 GURU",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
@@ -16,7 +16,7 @@ module.exports = {
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M NIGHT-HOWLER WHATSAPP BOT 😊♻️",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M GURU MD WHATSAPP BOT 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
@@ -24,14 +24,14 @@ module.exports = {
     FAKE_TYPING: process.env.FAKE_TYPING || "false",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
     CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
-    HEART_REACT: process.env.HEART_REACT || "false",
-    OWNER_REACT: process.env.OWNER_REACT || "false",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "923414812972",
-    OWNER_NAME: process.env.OWNER_NAME || "➺MS-HACKER࿐",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
+    HEART_REACT: process.env.HEART_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "+923017919788",
+    OWNER_NAME: process.env.OWNER_NAME || "➺5t7 GURU࿐",
     READ_CMD: process.env.READ_CMD || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺MS-HACKER࿐",
-    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү MS-HACKER`",
+    BOT_NAME: process.env.BOT_NAME || "➺5t7 GURU࿐",
+    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү 5t7 GURU",
     STATUS_REACT: process.env.STATUS_REACT || "true",
     ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
     ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'log' if you want to resend deleted message in ib chat 
